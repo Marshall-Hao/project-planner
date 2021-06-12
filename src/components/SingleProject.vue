28f0bbb7138fa0f25790952fca97d8545e8af1cb
marshall chen <marshallchan666@outlook.com>
2021-06-13 02:05:44 +0800

    projects show done with tailwind

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,36 @@
+<template>
+    <div @click="showDetails = !showDetails" class="bg-gray-100 px-5 py-5 space-y-4 rounded-sm shadow-sm border-l-4 border-red-500">
+        <div class="font-bold text-gray-600 text-md flex justify-between">
+            {{ project.title }}
+            <div class="flex space-x-3 cursor-pointer">
+                <span class="material-icons text-sm text-gray-400 hover:text-gray-600">
+                    edit
+                </span>
+                <span class="material-icons text-sm text-gray-400 hover:text-gray-600">
+                    delete
+                </span>
+                <span class="material-icons text-sm text-gray-400 hover:text-gray-600">
+                    done
+                </span>
+            </div>
+        </div>
+        <div v-if="showDetails"   class="font-medium text-gray-400 text-xs">
+            {{ project.details }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['project'],
+    data() {
+        return {
+            showDetails: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,31 @@
 <template>
+  <div class="justify-self-center mt-14 w-3/5">
+    <div v-if="projects.length" class="space-y-4">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project"/>
+      </div>  
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import SingleProject from '../components/SingleProject.vue'
 
 export default {
   name: 'Home',
   components: {
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
   }
 }
 </script>
